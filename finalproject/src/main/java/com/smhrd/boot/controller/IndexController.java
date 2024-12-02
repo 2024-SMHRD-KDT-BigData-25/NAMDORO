a7a16b439647316844af2642878bf06ef774521a
smhrd <smhrd@smart>
2024-12-02 17:33:34 +0900

    김민규) 수정 : indexcontroller 생성 : login.html, join.html, image폴더

--- finalproject/src/main/java/com/smhrd/boot/controller/IndexController.java
+++ finalproject/src/main/java/com/smhrd/boot/controller/IndexController.java
@@ -6,9 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 	
-	@GetMapping("/")
-	public String indexPage() {
-		return "index";
+	@GetMapping("/login")
+	public String loginPage() {
+		return "login";
+	}
+	
+	@GetMapping("/join")
+	public String joinPage() {
+		return "join";
 	}
 
 }
